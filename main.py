805120ae242a94b4ec0c43b8e3071b69a5ab835d
atrettvik <139919019+atrettvik@users.noreply.github.com>
2023-07-19 11:28:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -59,6 +59,20 @@ class Player(Ship):
         self.mask = pygame.mask.from_surface(self.ship_img)
         self.max_health = health
     
+class Enemy(Ship):
+    COLOR_MAP = {
+        "red": (RED_SPACE_SHIP, RED_LASER),
+        "green": (GREEN_SPACE_SHIP, GREEN_LASER),
+        "blue": (BLUE_SPACE_SHIP, BLUE_LASER)
+    }
+    
+    def __init__(self, x, y, color, health=100):
+        super().__init__(x, y, health)
+        self.ship_img, self.laser_img = self.COLOR_MAP[color]
+        self.mask = pygame.mask.from_surface(self.ship_img)
+        
+    def move(self, vel):
+        self.y += vel
         
 
 # GAME FUNCTIONS
@@ -66,10 +80,17 @@ class Player(Ship):
 def main():
     run = True
     fps = 60
-    level = 1
+    level = 0
     lives = 5
     main_font = pygame.font.SysFont("comicsans", 40)
+    
+ #ENEMY STATS
+    enemies = []
+    wave_length = 5
+    enemy_vel = 1
+    
     player_vel = 5
+    
     player = Player(450, 700)    
     
     clock = pygame.time.Clock()
@@ -83,14 +104,24 @@ def main():
         WIN.blit(lives_label, (10, 10))
         WIN.blit(level_label, (WIDTH - level_label.get_width() - 10, 10))
         
-        player.draw(WIN)
+        for enemy in enemies:
+            enemy.draw(WIN)        
+        
+        player.draw(WIN)      
         
         pygame.display.update()
         
 # EVENT LOOP   
     while run:
         clock.tick(fps)
-        redraw_window()
+        
+        if len(enemies) == 0:
+            level += 1
+            wave_length += 5
+            for i in range(wave_length):
+                enemy = Enemy(random.randrange(50, WIDTH-50), random.randrange(-1500, -100), random.choice(["red", "blue", "green"]))
+                enemies.append(enemy)  
+            
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -105,5 +136,12 @@ def main():
             player.y -= player_vel
         if keys[pygame.K_s] and player.y + player_vel + player.get_height() < HEIGHT: #DOWN
             player.y += player_vel
+            
+        for enemy in enemies:
+            enemy.move(enemy_vel)
+            
+            
+            
+        redraw_window()
 
 main()
