7c3a5fba21e86552617b779424b3ba0cd7db4ac1
atrettvik <139919019+atrettvik@users.noreply.github.com>
2023-07-19 08:08:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -16,16 +16,37 @@ pygame.display.set_caption("Alien")
 RED_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_red_small.png"))
 GREEN_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_green_small.png"))
 BLUE_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_blue_small.png"))
-# PLAYER SHIP
+
+# PLAYER SHIP IMAGE
 YELLOW_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_yellow.png"))
-# LASER
+
+# LASER IMAGE
 RED_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_red.png"))
 GREEN_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_green.png"))
 BLUE_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_blue.png"))
 YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"))
+
 # BACKGROUND
 BG = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background-black.png")), (WIDTH, HEIGHT))
 
+
+
+# SHIP FUNCTIONS
+class Ship:
+    def __init__(self, x, y, health=100):
+        self.x = x
+        self.y = y
+        self.health = health
+        self.ship_img = None
+        self.laser_img = None
+        self.lasers = []
+        self.cool_down_counter = 0
+        
+    def draw(self, window):
+        pygame.draw.rect(window, (255, 0, 0), (self.x, self.y, 50, 50))               
+            
+            
+            
 
 # GAME FUNCTIONS
 
@@ -34,7 +55,9 @@ def main():
     fps = 60
     level = 1
     lives = 5
-    main_font = pygame.font.SysFont("comicsans", 50)
+    main_font = pygame.font.SysFont("comicsans", 40)
+    
+    ship = Ship(300, 650)    
     
     clock = pygame.time.Clock()
     
@@ -46,6 +69,8 @@ def main():
         
         WIN.blit(lives_label, (10, 10))
         WIN.blit(level_label, (WIDTH - level_label.get_width() - 10, 10))
+        
+        ship.draw(WIN)
         
         pygame.display.update()
         
@@ -60,5 +85,4 @@ def main():
 
 
 
-main() 
-              
+main()
