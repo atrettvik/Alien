ac012d8b8206c3fc45c3f478beb29bafcec232c4
atrettvik <139919019+atrettvik@users.noreply.github.com>
2023-07-19 12:27:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -29,10 +29,31 @@ YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"
 # BACKGROUND
 BG = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background-black.png")), (WIDTH, HEIGHT))
 
-
-
+# LASER FUNCTIONS
+class Laser:
+    def __init__(self, x, y, img):
+        self.x = x
+        self.y = y
+        self,img = img
+        self.mast = pygame.mast.from_surface(self.img)
+    
+    def draw(self, window):
+        window.blit(self.img, (self.x, self.y))
+        
+    def move(self, vel):
+        self.y += vel
+        
+    def off_screen(self, height):
+        return self.y <= height and self.y >= 0
+    
+    def collision(self, obj):
+        return collide(obj, self)
+      
+        
 # SHIP FUNCTIONS
 class Ship:
+    COOLDOWN = 30
+
     def __init__(self, x, y, health=100):
         self.x = x
         self.y = y
@@ -44,11 +65,38 @@ class Ship:
         
     def draw(self, window):
         window.blit(self.ship_img, (self.x, self.y))
+        for laser in self.lasers:
+            laser.draw(window)
+            
+    def move_lasers(self, vel, obj):
+        self.coldown()
+        for laser in self.lasers:
+            laser.move(vel)
+            if laser.off_screen(HEIGHT):
+                self.lassers.remove(laser)
+            elif laser.collision(obj):
+                obj.health -= 10
+                self.lasers.remove(laser)
+            
+        
+    def cooldown(self):
+        if self.cool_down_counter >= self.COOLDOWN:
+            self.cool_down_counter = 0
+        elif self.cool_down_counter > 0:
+            self_cool_down_counter += 1
+    
+    def shoot(self):
+        if self.cool_down_counter == 0:
+            laser = Laser(x, y, self.laser_img)
+            self.lasers.append(laser)
+            self.cool_down_counter = 1
+            
         
     def get_width(self):
         return self.ship_img.get_width()               
     def get_height(self):
         return self.ship_img.get_height()
+          
     
             
 class Player(Ship):
@@ -73,6 +121,11 @@ class Enemy(Ship):
         
     def move(self, vel):
         self.y += vel
+        
+def collide(obj1, obj2):
+    offset_x = obj2.x - obj1.x
+    offset_y = obj2.y - obj1.y
+    return obj1.mask.overlap(obj2.mask, (offset_x, offset_y)) != None    
         
 
 # GAME FUNCTIONS
@@ -158,6 +211,10 @@ def main():
             player.y -= player_vel
         if keys[pygame.K_s] and player.y + player_vel + player.get_height() < HEIGHT: #DOWN
             player.y += player_vel
+        if keys[pygame.K_SPACE]:
+            player.shoot
+            
+            
             
         for enemy in enemies[:]:
             enemy.move(enemy_vel)
